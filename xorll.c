a71f7fe88e05b5215e97408b6c03ab7fd427cf8a
RinHizakura <s921975628@gmail.com>
2020-08-30 13:53:24 +0800

    Commit at the first time

---
+++ xorll.c
@@ -0,0 +1,112 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct __list list;
+struct __list {
+    int data;
+    struct __list *addr;
+};
+
+#define XOR(a, b) ((list *) ((uintptr_t) (a) ^ (uintptr_t) (b)))
+
+void insert_node(list **l, int d) {
+    list *tmp = malloc(sizeof(list));
+    tmp->data = d;
+
+    if (!(*l)) {
+        tmp->addr = NULL;
+    } else {
+        (*l)->addr = XOR(tmp, (*l)->addr);
+        tmp->addr = *l;
+    }
+    *l = tmp;
+}
+
+void delete_list(list *l) {
+    while (l) {
+        list *next = l->addr;
+        if (next)
+            next->addr = XOR(next->addr, l);
+        free(l);
+        l = next;
+    }
+}
+
+list *sort(list *start)
+{
+    if (!start || !start->addr)
+        return start;
+
+    list *left = start, *right = start->addr;
+    left->addr = NULL;
+    right->addr = XOR(right->addr, left);
+
+    left = sort(left);
+    right = sort(right);
+
+    for (list *merge = NULL; left || right;) {
+        if (!right || (left && left->data < right->data)) {
+            list *next = left->addr;
+            if (next)
+                next->addr = XOR(left, next->addr);
+
+            if (!merge) {
+                start = merge = left;
+                merge->addr = NULL;
+            } else {
+                merge->addr = XOR(merge->addr, left);
+                left->addr = merge;
+                merge = left;
+            }
+            left = next;
+        } else {
+            list *next = right->addr;
+            if (next)
+                next->addr = XOR(right, next->addr);
+
+            if (!merge) {
+                start = merge = right;
+                merge->addr = NULL;
+            } else {
+                merge->addr = XOR(merge->addr, right);
+                right->addr = merge;
+                merge = right;
+            }
+            right = next;
+        }
+    }
+
+    return start;
+}
+
+void list_print(list *l)
+{
+    printf("%d ",l->data);
+    list *next = l->addr;
+    list *prev = NULL;
+
+    while (next != NULL) {
+        printf("%d ",next->data);
+	prev = l;
+	l = next;
+        next = XOR(next->addr, prev); 
+    }
+    printf("\n");
+}
+
+int main(){
+    list *l = NULL;
+    insert_node(&l, 5);
+    insert_node(&l, 3);
+    insert_node(&l, 10);
+    insert_node(&l, 1);
+        
+    
+    printf("Before: "); 
+    list_print(l);
+    l = sort(l);
+    printf("After: "); 
+    list_print(l);
+    delete_list(l);
+}
