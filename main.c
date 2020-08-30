3196ba8f6b2cf036ff4426c5f212fc829d317e81
RinHizakura <s921975628@gmail.com>
2020-08-31 00:43:54 +0800

    Implement efficient merge sort

--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 int main()
 {
     list *l = NULL;
-    for (int i = 0; i < 50; i++) { /* generate random list */
+    
+    for (int i = 0; i < 50; i++) { 
         insert_node(&l, (rand() % 100));
     }
+    
 
     printf("Before: ");
     dump_list(l);
-    l = insertion_sort(l);
+    l = merge_sort(l);
     printf("After: ");
     dump_list(l);
     delete_list(&l);
